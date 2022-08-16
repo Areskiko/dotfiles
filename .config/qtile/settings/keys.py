1f5e68a49b16a52104640fcd2dd9acab7247c7fe
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-08-16 13:06:55 +0200

    Add flameshot

--- .config/qtile/settings/keys.py
+++ .config/qtile/settings/keys.py
@@ -89,4 +89,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     # Brightness
     ([], "XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 10")),
     ([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 10")),
+    
+    # Other
+    ([], "Print", lazy.spawn("flameshot gui"))
 ]]

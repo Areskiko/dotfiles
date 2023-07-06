bf68e3faee9268d68538889c866692238ca76897
Stefan Mack <git.mack@proton.me>
2023-07-06 13:24:18 +0200

    Add temporary screenshot

--- .config/sway/screenshot.sh
+++ .config/sway/screenshot.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+MODE=$1
 DATE=$(date +'screenshot_%Y-%m-%d-%H%M%S.png')
-SCREENSHOT_PATH=$(xdg-user-dir SCREENSHOTS)/$DATE
+if [ $1 == "save" ]
+then
+	SCREENSHOT_PATH=$(xdg-user-dir SCREENSHOTS)/$DATE
+else
+	SCREENSHOT_PATH=/tmp/$DATE
+fi
 
 slurp | grim -g - $SCREENSHOT_PATH
 wl-copy < $SCREENSHOT_PATH
